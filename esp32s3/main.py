01d570a943113778db6a4769800fdad999a8c9ed
SpencerMarquardt <spencer.marquardt@whoi.edu>
2025-08-14 14:57:13 -0400

    Simplify sensor initialization by removing multiplexer usage

--- esp32s3/main.py
+++ esp32s3/main.py
@@ -5,23 +5,23 @@ from lib import adafruit_MPRLS
 from lib.tca9548a import PCA9546A
 
 # Base I2C bus
-i2c = machine.I2C(0, sda=machine.Pin(41), scl=machine.Pin(40))
+i2c = machine.I2C(0, sda=machine.Pin(8), scl=machine.Pin(9))
 
 # Init multiplexer
-mux = PCA9546A(i2c)
+# mux = PCA9546A(i2c)
 
 # Create sensor instances using channel 0 and 1
-mprls0 = adafruit_MPRLS.MPRLS.create_instance(mux[0])
-mprls1 = adafruit_MPRLS.MPRLS.create_instance(mux[3])
+mprls0 = adafruit_MPRLS.MPRLS.create_instance(i2c)
+# mprls1 = adafruit_MPRLS.MPRLS.create_instance(mux[3])
 
 while True:
     try:
         p0 = mprls0.pressure
-        p1 = mprls1.pressure
+        # p1 = mprls1.pressure
 
         payload = {
             "CH0": p0,
-            "CH1": p1,
+            # "CH1": p1,
         }
         print(ujson.dumps(payload))
 
